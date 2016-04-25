fea5ef19d58c5a25a400ccbf5266a38e06172db3
Viny <Viny@Diego>
2016-04-25 15:20:04 -0500

    Segunda modificacion

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -18,6 +18,7 @@ public class Prueba {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo....");
+        System.out.println("hola a todos");
     }
     
 }
